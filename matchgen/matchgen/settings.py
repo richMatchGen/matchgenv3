178c72b4341b54e6e5df0d84496e301c9293c28e
richMatchGen <rich@matchgen.co.uk>
2025-03-15 13:03:07 +0000

    dburl

--- matchgen/matchgen/settings.py
+++ matchgen/matchgen/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -145,14 +146,7 @@ WSGI_APPLICATION = 'matchgen.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'matchgen_db',
-        'USER': 'matchgen_user',
-        'PASSWORD': '@Vale1923',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
