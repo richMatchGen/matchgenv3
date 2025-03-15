54231b8556e66af797828ce7e3f627d52f884202
richMatchGen <rich@matchgen.co.uk>
2025-03-15 15:13:40 +0000

    Moved Procfile to root and updated start command"

--- matchgenBackend/matchgen/asgi.py
+++ matchgenBackend/matchgen/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for matchgen project.
+ASGI config for matchgenBackend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'matchgen.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'matchgenBackend.settings')
 
 application = get_asgi_application()
