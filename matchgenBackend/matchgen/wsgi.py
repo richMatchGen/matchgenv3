ceec0aa92f9cf4c0fcbd71d5717b9658e2691bcd
richMatchGen <rich@matchgen.co.uk>
2025-03-15 16:23:51 +0000

    Ensured wsgi.py exists

--- matchgenBackend/matchgen/wsgi.py
+++ matchgenBackend/matchgen/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'matchgenBackend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'matchgen.settings')
 
 application = get_wsgi_application()
