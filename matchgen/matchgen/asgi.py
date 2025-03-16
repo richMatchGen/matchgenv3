0a4405c3b4347fa33e9f8449cdd1c23cac086460
richMatchGen <rich@matchgen.co.uk>
2025-03-16 15:36:19 +0000

    Ensured wsgi.py exists

--- matchgen/matchgen/asgi.py
+++ matchgen/matchgen/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for matchgenBackend project.
+ASGI config for matchgen project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- matchgen/matchgen/wsgi.py
+++ matchgen/matchgen/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for matchgenBackend project.
+WSGI config for matchgen project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
