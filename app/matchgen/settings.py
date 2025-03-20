9c5c770457f68dbe98695c1e5a5221ac1da6cfcc
richMatchGen <rich@matchgen.co.uk>
2025-03-20 06:42:04 +0000

    requirements.txt

--- app/matchgen/settings.py
+++ app/matchgen/settings.py
@@ -151,7 +151,7 @@ WSGI_APPLICATION = 'matchgen.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-'default': dj_database_url.config(default="postgresql://postgres:xsqsnrEfrbolZPTbhKZHotfNnmVXzOgv@postgres.railway.internal:5432/railway")
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
 
